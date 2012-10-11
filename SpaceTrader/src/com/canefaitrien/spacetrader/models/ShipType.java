08c08cf27dc814f9738f3fbd63c2e17df1c549d9
An Pham <arian.pham@gatech.edu>
2012-10-11 16:54:17 -0400

    Try to see if Git is working

--- SpaceTrader/src/com/canefaitrien/spacetrader/models/ShipType.java
+++ SpaceTrader/src/com/canefaitrien/spacetrader/models/ShipType.java
@@ -3,38 +3,38 @@ package com.canefaitrien.spacetrader.models;
 public enum ShipType {
 	// Gnat 
 	// speed of 14 parsecs, 1 weapon, 1 gadget, 15 cargo hold
-	Gnat,
+	Gnat(14, 100, 1, 1, 0, 15, 0, 0, "Gnat"),
 	// Flea
 	// small, few cargo holds, a weak hull and no equipment, 20 parsecs/tank, 
 	// can be converted from a escape pod
-	Flea,
+	Flea(17, 100, 1, 1, 1, 20, 0, 0, "Flea"),
 	// Firefly
 	// cheap, 1 of each equipment type slot, 17 parsecs/tank
 	// 20 cargo hold
-	Firefly,
+	Firefly(17, 200, 1, 1, 1, 20, 0, 0, "Firefly"),
 	// Mosquito
 	// 13 parsec/tank, strong hull, 2 weapon, 1 shield, 1 gadget, 15 cargo hold
-	Mosquito,
+	Mosquito(15, 300, 2, 1, 1, 15, 0, 0, "Mosquito"),
 	// Bumblebee
 	//20 cargo hold, hull = fifrefly, 2 shield, 2 gadget, mercernary available 15/tank
-	Bumblebee,
+	Bumblebee(15, 200, 0, 2, 2, 20, 0, 5, "Bumblebee"),
 	// Beetle
 	// weak hull no weapon, 1 shield, 1 gadget, 50 cargo, 14 parsec/tank
-	Beetle,
+	Beetle(14, 100, 0, 1, 1, 50, 0, 0, "Beetle"),
 	// Hornet
 	// Strong hull, 3 weapon, 2 shield, 1 gadget, 20 cargo, 16/tank
-	Hornet,
+	Hornet(16, 300, 3, 1, 2, 20, 0, 0, "Hornet"),
 	// Grasshopper
 	// 30 cargo/ 2 weapons, 2 shield, 3 gadget, 3 crew, 15/tank
-	Grasshopper,
+	Grasshopper(15, 200, 2, 3, 2, 30, 3, 0, "Grasshopper"),
 	// Termite
 	// Strong hull, 3 shield, 2 gadgets, 3 crew, 60 cargo, 1 weapon, 13/tank
-	Termite,
+	Termite(13, 300, 1, 2, 3, 60, 3, 0, "Termite"),
 	// Wasp
 	// Utimalte private ship, strong hull, 3 weapons, 2 shield, 2 gadget, 3 crew, 14/tank, 35 cargo
 	Wasp(14, 300, 3, 2, 2, 35, 3, 0, "Wasp"),
 	// Canefaitrien
-	Canefaitrien // Ultimate fighting, trading ship
+	Canefaitrien(1000, 3000, 300, 200, 200, 35000, 10, 10, "Canefaitrien") // Ultimate fighting, trading ship
 	;
 	
 
@@ -48,8 +48,6 @@ public enum ShipType {
 	private int maxHullStrength;
 	private String type;
 	
-	ShipType() {	
-	}
 	
 	ShipType (	int maxDistance, int maxHullStrength,
 				int maxWeaponSlots, int maxGadgetSlots, int maxShieldSlots,
--- SpaceTrader/src/com/canefaitrien/spacetrader/models/TradeGood.java
+++
@@ -1,7 +0,0 @@
-package com.canefaitrien.spacetrader.models;
-
-import com.canefaitrien.spacetrader.interfaces.IModel;
-
-public abstract class TradeGood implements IModel{
-
-}
