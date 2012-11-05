3ddac52329b225562c62309a838ce89b58d496ad
An Pham <arian.pham@gatech.edu>
2012-11-04 19:29:24 -0500

    Add extra button for encounter testing. More will be pushed shortly

--- SpaceTraderJava/src/view/CharacterCreationPanel.java
+++ SpaceTraderJava/src/view/CharacterCreationPanel.java
@@ -15,13 +15,16 @@ import model.Person;
 public class CharacterCreationPanel extends JPanel {
 	
 	private static final long serialVersionUID = 1227113222169913502L;
-	
+	Controller controller = new Controller(new Person("Bob", 0, 0, 0, 0), Difficulty.Easy);// This is added by An Pham on 11/04/12
 	JPanel mainPanel;
 	public CharacterCreationPanel(JPanel panel) {
 		JButton button = new JButton("Go To Marketplace");
 		this.mainPanel = panel;
 		button.addActionListener(new ButtonListener());
 		add(button);
+		JButton travelButton = new JButton("Travel");// This is added by An Pham on 11/04/12
+		travelButton.addActionListener(new TravelButtonListener());// This is added by An Pham on 11/04/12
+		add(travelButton);// This is added by An Pham on 11/04/12
 	}
 	
 	private class ButtonListener implements ActionListener {
@@ -30,6 +33,18 @@ public class CharacterCreationPanel extends JPanel {
 			mainPanel.removeAll();
 			mainPanel.add(new MarketplacePanel(new Controller(new Person("Bob", 0, 0, 0, 0), Difficulty.Easy)));
 			mainPanel.revalidate();
+		}
+	}
+	
+	// Below is added by An Pham on 11/04/12
+	private class TravelButtonListener implements ActionListener {
+		
+		public void actionPerformed(ActionEvent e) {
+			try {
+				controller.move(controller.getUniverse().getPlanets()[0]);
+			} catch(Exception ex) {
+				
+			}
 		}
 	}
 
