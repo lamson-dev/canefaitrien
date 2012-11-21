ee5373e6bf92f53c88dab772c1b12c8d27edbdc7
An Pham <arian.pham@gatech.edu>
2012-11-20 22:51:26 -0500

    Modification to my JUnitTest

--- SpaceTraderJava/src/other/AnPham_JunitTest.java
+++ SpaceTraderJava/src/other/AnPham_JunitTest.java
@@ -9,7 +9,7 @@ import controller.Controller;
 import controller.Controller.Difficulty;
 
 /**
- * Junit Testing for Ship.addGood and PirateEncounter.takeGoods to see if they are working correctly together
+ * Junit Testing for PoliceEncounter.takeGoods
  * @author An Pham
  * @Date 11/14/12
  * @Version 1.0
@@ -19,68 +19,174 @@ public class AnPham_JunitTest {
 
 	// Setting up the necessary data
 	Controller data;
+	// Initialization
 	private void settingUp() {
 		data = new Controller(new Person("Apham9", 0, 0, 0, 0), Difficulty.Easy);
 	}
-	
-	
-	// Check Ship.addGood functioning to make sure it works before checking PirateEncounter.takeGoods
+
+
+	// Assuming the Ship.addGood is functioning correctly, check to see if PoliceEncounter.checkGoods actually take care of illegal goods
+	// Illegal Goods are fire arm and narcotics
+	// When there is presence of illegal good, they will be taken, and the play will be charge the fine up to 20% of their current money. 
+
+	// This will test the presence of fire arm only
 	@Test(timeout = 2000)
-	public void CheckAddGoodMethod() {
+	public void checkFireArm() {
 		settingUp();
 		try {
 			data.getShip().addGood(TradeGood.GAMES);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		try {
 			data.getShip().addGood(TradeGood.FIREARMS);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		int[] current = new int[TradeGood.values().length];
+
 		for (int i = 0; i < current.length; i++) 
 			current[i] = 0;
-		
+
 		current[TradeGood.GAMES.ordinal()] = 1;
 		current[TradeGood.FIREARMS.ordinal()] = 1;
-		
+
 		Assert.assertArrayEquals("Fail adding good item to their correct locations", current, data.getShip().getCargo());
-		
-		
+
+		int moneyAfterFine = data.getMoney() * 8 / 10;
+		PoliceEncounter police = new PoliceEncounter(data);
+		police.checkGoods();
+
+		Assert.assertEquals("Fail to charge the fine", moneyAfterFine, data.getMoney());
+
+		current[TradeGood.FIREARMS.ordinal()] = 0;
+
+		Assert.assertArrayEquals("Fail to take illegal goods away", current, data.getShip().getCargo());
 	}
-	
-	
-	// Assuming the Ship.addGood is functioning correctly, check to see if PirateEncounter.takeGoods actually take all goods in the cargo
+
+
+	// This will test the presence of fire arm only
 	@Test(timeout = 2000)
-	public void CheckPirateTakeGoodsMethod() {
+	public void checkNarcotics() {
 		settingUp();
 		try {
-			data.getShip().addGood(TradeGood.GAMES);
+			data.getShip().addGood(TradeGood.WATER);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			data.getShip().addGood(TradeGood.NARCOTICS);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		int[] current = new int[TradeGood.values().length];
+
+		for (int i = 0; i < current.length; i++) 
+			current[i] = 0;
+
+		current[TradeGood.WATER.ordinal()] = 1;
+		current[TradeGood.NARCOTICS.ordinal()] = 1;
+
+		Assert.assertArrayEquals("Fail adding good item to their correct locations", current, data.getShip().getCargo());
+
+		int moneyAfterFine = data.getMoney() * 8 / 10;
+		PoliceEncounter police = new PoliceEncounter(data);
+		police.checkGoods();
+
+		Assert.assertEquals("Fail to charge the fine", moneyAfterFine, data.getMoney());
+
+		current[TradeGood.NARCOTICS.ordinal()] = 0;
+
+		Assert.assertArrayEquals("Fail to take illegal goods away", current, data.getShip().getCargo());
+		Assert.assertEquals("Fail to retain other legal trade goods", 1, (data.getShip().getCargo())[TradeGood.WATER.ordinal()]);
+	}
+
+	// This will test the presence of fire arm only
+	@Test(timeout = 2000)
+	public void checkFireArmAndNarcotics() {
+		settingUp();
+		try {
+			data.getShip().addGood(TradeGood.FUR);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			data.getShip().addGood(TradeGood.NARCOTICS);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		try {
 			data.getShip().addGood(TradeGood.FIREARMS);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		int[] current = new int[TradeGood.values().length];
+
 		for (int i = 0; i < current.length; i++) 
 			current[i] = 0;
-		
-		PirateEncounter pirate = new PirateEncounter(data);
-		pirate.takeGoods();
-		
-		Assert.assertArrayEquals("Fail to take all the goods from cargo hold", current, data.getShip().getCargo());
-		
-		
+
+		current[TradeGood.NARCOTICS.ordinal()] = 1;
+		current[TradeGood.FIREARMS.ordinal()] = 1;
+		current[TradeGood.FUR.ordinal()] = 1;
+
+		Assert.assertArrayEquals("Fail adding good item to their correct locations", current, data.getShip().getCargo());
+
+		int moneyAfterFine = data.getMoney() * 8 / 10;
+		PoliceEncounter police = new PoliceEncounter(data);
+		police.checkGoods();
+
+		Assert.assertEquals("Fail to charge the fine", moneyAfterFine, data.getMoney());
+
+		current[TradeGood.FIREARMS.ordinal()] = 0;
+		current[TradeGood.NARCOTICS.ordinal()] = 0;
+
+		Assert.assertArrayEquals("Fail to take illegal goods away", current, data.getShip().getCargo());
+		Assert.assertEquals("Fail to retain other legal trade goods", 1, (data.getShip().getCargo())[TradeGood.FUR.ordinal()]);
 	}
 	
+	// This will test the none presence of illegal trade good only
+	@Test(timeout = 2000)
+	public void checkNoIllegalTradeGood() {
+		settingUp();
+		try {
+			data.getShip().addGood(TradeGood.FUR);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			data.getShip().addGood(TradeGood.FOOD);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			data.getShip().addGood(TradeGood.MACHINES);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		int[] current = new int[TradeGood.values().length];
+
+		for (int i = 0; i < current.length; i++) 
+			current[i] = 0;
+
+		current[TradeGood.FUR.ordinal()] = 1;
+		current[TradeGood.FOOD.ordinal()] = 1;
+		current[TradeGood.MACHINES.ordinal()] = 1;
+
+		Assert.assertArrayEquals("Fail adding good item to their correct locations", current, data.getShip().getCargo());
+
+		int moneyAfterFine = data.getMoney();
+		PoliceEncounter police = new PoliceEncounter(data);
+		police.checkGoods();
+
+		Assert.assertEquals("Fail to keep the money intact", moneyAfterFine, data.getMoney());
+
+		Assert.assertEquals("Fail to retain other legal trade goods", 1, (data.getShip().getCargo())[TradeGood.FOOD.ordinal()]);
+		Assert.assertEquals("Fail to retain other legal trade goods", 1, (data.getShip().getCargo())[TradeGood.FUR.ordinal()]);
+		Assert.assertEquals("Fail to retain other legal trade goods", 1, (data.getShip().getCargo())[TradeGood.MACHINES.ordinal()]);
+	}
+
 }
